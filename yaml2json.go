73a2494d581f82ec410d9e9833db769d3c293695
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2019-02-11 10:21:10 -0800

    Create yaml2json.go

---
+++ yaml2json.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+func main() {
+	var text string
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text = text + scanner.Text()
+	}
+
+	if scanner.Err() != nil {
+    fmt.Println("Unable to read input")
+		os.Exit(1)
+	}
+
+	var object interface{}
+	_ = yaml.Unmarshal([]byte(text), &object)
+
+	o, _ := json.Marshal(&object)
+	fmt.Println(string(o))
+}
